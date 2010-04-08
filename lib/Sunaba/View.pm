493c1a1bc6ffedfe06155fefcba339b9b123a4dc
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-04-08 12:21:25 -0700

    added notice

--- lib/Sunaba/View.pm
+++ lib/Sunaba/View.pm
@@ -16,6 +16,9 @@ my $layout = sub {
                 div('.description') {
                     "Sunaba runs your Plack/PSGI apps in the cloud.";
                 };
+                div('.error') {
+                    "4/8 12pm PDT: Due to the massive traffic, the backend is tentatively blocking us. Sorry for the inconvenience.";
+                };
 
                 div('.mainbody') {
                     outs_raw $content->();
